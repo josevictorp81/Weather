d83199658da64b9744215fa45650ff02317c1d20
josevictorp81 <josevictorp81@gmail.com>
2021-06-08 21:44:29 -0300

    mensagem para informar uma cidade

--- weather/core/views.py
+++ weather/core/views.py
@@ -5,13 +5,11 @@ from django.conf import settings
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        #country = request.POST['country']
 
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=pt_br&appid={settings.OPEN_WEATHER_MAP_API_KEY}'
         
-
         result = requests.get(url=url).json()
-        print(result)
+        #print(result)
         weather = {
             'city': city,
             'temperature': result['main']['temp'],
@@ -19,7 +17,6 @@ def index(request):
             'icon': result['weather'][0]['icon'],
             'place': result['sys']['country'],
         }
-        #print(weather)
 
         data = {
             'weather': weather,
